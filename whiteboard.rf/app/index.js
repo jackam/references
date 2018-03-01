5de44790e4c885b36c080764e2dbde95aca38bf0
Jack AM <hi@jackam.me>
2018-03-01 08:01:27 -1000

    Added a whiteboard directory.

---
+++ whiteboard.rf/app/index.js
@@ -0,0 +1,8 @@
+/*
+ * index.js
+ */
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouterDOM = require('react-router-dom');
+var BrowserRouter = RouterReactDOM.BrowserRouter;
